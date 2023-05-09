4fa7dd93da6376901c92d5d4388868fc30c6f81a
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2023-05-09 19:54:05 +0200

    refactor: update imports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { BackgroundWrapper } from './backgroundWrapper';
 export { MusicList } from './musicList';
 export { Accordion } from './Accordion';
 export { AccordionItem } from './AccordionItem';
+export { AccordionItemController } from './AccordionItemController';
