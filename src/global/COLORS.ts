cc9adf7ec48dc9c34f5c27df4477002d1a46a790
Mateusz <37371388+Zirek90@users.noreply.github.com>
2023-05-10 19:08:26 +0200

    style: add styles to navigation

--- src/global/COLORS.ts
+++ src/global/COLORS.ts
@@ -3,6 +3,7 @@ export const COLORS = {
   album_file_background: 'rgba(35, 35, 35, 0.8)',
   white: '#ffffff',
   black: '#000000',
+  navigation_top_background: '#0f0f0f',
   progress_bar_start: '#61ae5c',
   progress_bar_end: '#195614',
   control_play_inactive: '#ffffff',
