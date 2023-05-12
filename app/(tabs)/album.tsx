e5c4e16064f428556156dcf12a1600b83ea94c6b
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2023-05-12 18:35:00 +0200

    feat: update import paths with screens, reneme and move album list to screen directory

--- app/(tabs)/album.tsx
+++ app/(tabs)/album.tsx
@@ -1,9 +1,10 @@
-import { BackgroundWrapper, MusicList } from '@components';
+import { BackgroundWrapper } from '@components';
+import { AlbumList } from '@screens';
 
 const Album = () => {
   return (
     <BackgroundWrapper>
-      <MusicList />
+      <AlbumList />
     </BackgroundWrapper>
   );
 };
