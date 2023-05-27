c08c86128702632e010bf73f58b74f347e968659
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2023-05-27 18:35:57 +0200

    feat: add hoc for memorizing musicContext data and implement in SongItemControllers

---
+++ src/hoc/withMusicContext.tsx
@@ -0,0 +1,21 @@
+import { ComponentType, MemoExoticComponent, memo } from 'react';
+import { useMusicContext, ContextState } from '@context';
+
+export const withMusicContext = <TProps extends unknown, TValue extends unknown>(
+  Component: ComponentType<TProps & Record<string, TValue>>,
+  selectors: Record<string, (data: ContextState) => TValue>,
+): any => {
+  const MemorisedComponent = memo(Component) as MemoExoticComponent<
+    ComponentType<Record<string, TValue>>
+  >;
+
+  return (props: TProps & Record<string, TValue>) => {
+    const data = useMusicContext();
+    const contextProps = Object.keys(selectors).reduce((acc: Record<string, TValue>, key) => {
+      acc[key] = selectors[key](data);
+      return acc;
+    }, {});
+
+    return <MemorisedComponent {...props} {...contextProps} />;
+  };
+};
