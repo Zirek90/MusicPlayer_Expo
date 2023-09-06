a7ddc974ffac1982ca8c2307f40395341303c741
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2023-09-06 11:46:17 +0200

    test: add mock for asyncStorage

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler/jestSetup';
-import mockAsyncStorage from './mocks/asyncStorage';
+import mockAsyncStorage from './__mocks__/asyncStorage';
 
 // Mock AsyncStorage
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
